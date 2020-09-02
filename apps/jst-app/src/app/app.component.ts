4bcf6917e4ec9e7e6c9a7a072676a643ccc1a152
admir86 <admir.ramic@outlook.com>
2020-09-02 20:59:56 +0200

    init

---
+++ apps/jst-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jst-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'jst-app';
+}
